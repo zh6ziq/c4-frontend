278fc336bf68cf40f7ffcda60ca317a8d16d83f5
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2021-04-02 04:30:10 +0800

    add MainLayout and AdminLayout

---
+++ src/layout/AdminLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-info">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>Admin</q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Klinik Meow</q-item-label>
+        <q-item clickable tag="a" href="/admin">
+          <q-item-section avatar>
+            <q-icon name="dashboard" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Dashboard</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" href="/patients">
+          <q-item-section avatar>
+            <q-icon name="person" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Patient</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" href="/appointment">
+          <q-item-section avatar>
+            <q-icon name="event" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Appointment</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable tag="a" href="/medicine">
+          <q-item-section avatar>
+            <q-icon name="medication" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Medicine</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: "LayoutDefault",
+
+  components: {},
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-primary">
+      <q-toolbar>
+        <q-toolbar-title>Meow Meow</q-toolbar-title>
+
+        <div>
+          <q-tabs shrink>
+            <q-route-tab name="tab1" label="Home" to="/" />
+            <q-route-tab name="tab2" label="Contact" to="/contact" />
+          </q-tabs>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+
+      <q-footer>
+        <q-card square class="bg-dark">
+          <q-card-section class="flex flex-center">
+            <q-item-label>This is footer Meow Meow</q-item-label>
+          </q-card-section>
+        </q-card>
+      </q-footer>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: "LayoutDefault",
+
+  components: {},
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
+
+<style></style>
