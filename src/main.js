9253c89f1e4bb9d56087398f8ca84f1b5379f361
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2021-04-02 04:29:12 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./quasar";
+
+import axios from 'axios'
+
+Vue.prototype.$http = axios.create({
+  baseURL: process.env.VUE_APP_BACKEND_API
+})
 
 Vue.config.productionTip = false;
 
