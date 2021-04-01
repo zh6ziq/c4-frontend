b84a6eb343769bdfa361b3bd80ebd2b8e0c87cb0
zh6ziq <71532216+zh6ziq@users.noreply.github.com>
2021-04-02 04:34:36 +0800

    add admin dashboard page

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <q-card flat>
+      <q-card-section>
+        <q-card flat>
+          <q-card-section>
+            <q-item-label class="text-h6">This is admin dashboard</q-item-label>
+          </q-card-section>
+        </q-card>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {}
+</script>
